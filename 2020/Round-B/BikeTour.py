a63fef5f8568e9262bedaa5f030d5b517d8a6a53
nawue <negd1@alu.ua.es>
2020-09-23 01:11:16 +0200

    Countdown.py

--- 2020/Round-B/BikeTour.py
+++ 2020/Round-B/BikeTour.py
@@ -5,16 +5,16 @@ def Solve(H):
             if (H[i-1] < H[i] and H[i+1] < H[i] ):
                 res+=1
                 i+=1
-    return res       
+    return res
 
 
 
 def main():
     cases = int(input())
     for case in range(cases):
-        N = [int(_) for _ in input().split()]      
+        N = [int(_) for _ in input().split()]
         H = [int(_) for _ in input().split()]
-        print("Case #{}: {}".format(case + 1, Solve(H)))       
+        print("Case #{}: {}".format(case + 1, Solve(H)))
 
 if __name__ == '__main__':
     main()
--- 2020/Round-B/test.py
+++ 2020/Round-B/test.py
@@ -1,4 +1,7 @@
 x = [1,2,3,4,5,6,7,8]
+asdasd = 2
+
+asd
 
 for xi in x[::-1]:
        print(xi)
